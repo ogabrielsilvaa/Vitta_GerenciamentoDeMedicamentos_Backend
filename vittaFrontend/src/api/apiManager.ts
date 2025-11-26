08c72b21f07924a5215744961d03cadbb8dec5c4
oj0rel <ogabrieusiuva@gmail.com>
2025-11-26 10:55:54 -0300

    Alterações para gerar APK conectado ao backend.

--- vittaFrontend/src/api/apiManager.ts
+++ vittaFrontend/src/api/apiManager.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://academico3.rj.senac.br/vitta'
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL || "http://academico3.rj.senac.br/vitta";
 
 export const apiManager = axios.create({
   baseURL: BASE_URL,
